09d8797ad233a0a9c947550ae911a489b1e3befc
Arjumand Rukhsar <162131925+ArjumandRukhsar786@users.noreply.github.com>
2024-11-22 11:40:53 +0500

    changes done

--- app/fetch-posts/page.tsx
+++ app/fetch-posts/page.tsx
@@ -21,12 +21,29 @@ export default function FetchPostsPage() {
       .finally(() => setLoading(false));
   }, []);
 
+  if (loading) {
+    return (
+      <div className="container mx-auto p-6 text-center">
+        <h1 className="text-3xl font-semibold text-blue-600">Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="container mx-auto p-6 text-center">
+        <h1 className="text-3xl font-semibold text-red-600">{error}</h1>
+      </div>
+    );
+  }
+
   return (
     <div  className="container mx-auto p-6">
-      <h1  className="text-3xl font-semibold text-center mb-6 text-blue-600 underline hover:underline-offset-8">Posts</h1>
-      <ul  className="space-y-4">
+      <h1  className="text-3xl font-semibold text-center mb-6 text-blue-600 underline hover:underline-offset-8">
+        Latest Posts</h1>
+      <ul  className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map((post: { id: number; title: string; body: string }) => (
-          <li key={post.id} className="bg-white p-4 rounded-lg shadow-lg hover:bg-gray-200">
+          <li key={post.id} className="bg-pink-300 p-4 rounded-lg shadow-lg hover:bg-gray-200">
              <h2 className="text-xl font-semibold text-fuchsia-700">{post.title}</h2>
              <p className="text-gray-600 mt-2">{post.body}</p>
             </li>
